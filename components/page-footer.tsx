b110e59e56faade5059708c72417b5de1a124a7d
genesluna <genesluna@gmail.com>
2024-07-14 15:34:56 -0300

    feat: add page footer component

---
+++ components/page-footer.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import HorizontalLogo from '@assets/logo_hor.svg';
+import SocialIcons from './ui/social-icons';
+import Image from 'next/image';
+
+export default function PageFooter() {
+  return (
+    <footer className='flex h-56 w-full flex-col items-center justify-center rounded-t-4xl bg-accent'>
+      <Link href='/'>
+        <Image src={HorizontalLogo} alt='Logo' width={147} height={56} />
+      </Link>
+
+      <SocialIcons />
+
+      <p className='mt-6 text-sm text-white'>
+        © 2024. Todos os direitos reservados.
+      </p>
+    </footer>
+  );
+}
