1e75a10ab077bb6531c3ff5a519a3ec95a42166a
Genes Luna <genesluna@gmail.com>
2024-07-16 20:03:40 -0300

    feat: add pet card (#18)
    * feat: add pet card skeleton
    
    * feat: add pet card
    
    * feat: add pet model
    
    * feat: add func to get age based on date range
    
    * feat: add image skeleton
    
    * fix: fixed import
    
    * fix: added interface to new file

---
+++ components/pet-card-skeleton.tsx
@@ -0,0 +1,17 @@
+import ImageSkeleton from './ui/image-skeleton';
+
+export default function PetCardSkeleton() {
+  return (
+    <div className='h-[310px] w-auto animate-pulse rounded-3xl bg-white p-3 transition duration-300 ease-in-out hover:-translate-y-2 hover:shadow-xl'>
+      <div className='relative h-[200px] w-full'>
+        <ImageSkeleton />
+      </div>
+
+      <div className='px-3 pt-2'>
+        <div className='mt-1 h-4 w-56 rounded-full bg-gray-300' />
+        <div className='mt-2 h-4 w-56 rounded-full bg-gray-300' />
+        <div className='mt-2 h-4 w-56 rounded-full bg-gray-300' />
+      </div>
+    </div>
+  );
+}
