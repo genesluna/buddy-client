2bd2a6e80eeb3c845c601d29be31790d038557c0
genesluna <genesluna@gmail.com>
2024-07-19 17:23:59 -0300

    feat: add login page

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,12 @@
+import HorizontalLayout from '@components/layouts/horizontal-layout';
+import LoginForm from '@components/forms/login-form';
+
+export default function LoginPage() {
+  return (
+    <HorizontalLayout>
+      <h1 className='text-4xl font-bold text-accent'>Bem-vindo</h1>
+      <p className='mt-2 text-lg text-content-300'>Acesse a sua conta</p>
+      <LoginForm />
+    </HorizontalLayout>
+  );
+}
