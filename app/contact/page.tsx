9ce5b29ff1ca16718e9697032ae4b19cabea984e
genesluna <genesluna@gmail.com>
2024-07-19 17:24:32 -0300

    feat: add contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+import ContactForm from '@components/forms/contact-form';
+import HorizontalLayout from '@components/layouts/horizontal-layout';
+
+export default function ContactPage() {
+  return (
+    <HorizontalLayout>
+      <h1 className='text-4xl font-bold text-accent'>Contato</h1>
+      <p className='mt-2 text-lg text-content-300'>Escreva sua mensagem</p>
+      <ContactForm />
+    </HorizontalLayout>
+  );
+}
