14fb0ce52ed2fb8799378d7ece31e151b5d18bc1
genesluna <genesluna@gmail.com>
2024-07-23 17:20:09 +0000

    refactor: testing setup

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,27 @@
 import '@testing-library/jest-dom';
+
+// Mock the IntersectionObserver, see https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+export class IntersectionObserver {
+  root = null;
+  rootMargin = '';
+  thresholds = [];
+
+  disconnect() {
+    return null;
+  }
+
+  observe() {
+    return null;
+  }
+
+  takeRecords() {
+    return [];
+  }
+
+  unobserve() {
+    return null;
+  }
+}
+
+window.IntersectionObserver = IntersectionObserver;
+global.IntersectionObserver = IntersectionObserver;
