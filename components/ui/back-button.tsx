196a9bdd5f5d805e8f2969afc0b6bbc9f7cb2652
genesluna <genesluna@gmail.com>
2024-07-23 20:08:09 +0000

    refactor: move to use router.

--- components/ui/back-button.tsx
+++ components/ui/back-button.tsx
@@ -1,17 +1,16 @@
 'use client';
 
 import { ArrowCircleLeft } from '@phosphor-icons/react/dist/ssr';
+import { useRouter } from 'next/navigation';
 
 export default function BackButton() {
-  function previous() {
-    window.history.back();
-  }
+  const router = useRouter();
 
   return (
     <ArrowCircleLeft
       size={40}
       className='absolute left-8 top-11 cursor-pointer text-white'
-      onClick={previous}
+      onClick={() => router.back()}
     />
   );
 }
