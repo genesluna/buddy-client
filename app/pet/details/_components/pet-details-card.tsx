52a185713c85eaa9d22eb5fa25e707a126aa1e84
genesluna <genesluna@gmail.com>
2025-12-22 01:58:43 -0300

    Refactor pet-details-card component to use icon components from @phosphor-icons/react
    - Replaced CalendarBlank, MapPin, and PawPrint with their respective icon components: CalendarBlankIcon, MapPinIcon, and PawPrintIcon.
    - Adjusted class names for better readability and consistency in JSX structure.

--- app/pet/details/_components/pet-details-card.tsx
+++ app/pet/details/_components/pet-details-card.tsx
@@ -1,7 +1,7 @@
 import {
-  CalendarBlank,
-  MapPin,
-  PawPrint,
+  CalendarBlankIcon,
+  MapPinIcon,
+  PawPrintIcon,
 } from '@phosphor-icons/react/dist/ssr';
 import { birthDateToHumanReadableAge } from '@/app/_lib/utils';
 import Button from '@/app/_components/ui/button';
@@ -17,19 +17,19 @@ interface PetDetailsCardProps {
 export default function PetDetailsCard({ pet }: PetDetailsCardProps) {
   return (
     <div className='min-h-[480px] w-full rounded-[1.875rem] bg-white px-[2.5rem] py-[1.875rem] xl:mt-[-4.3125rem]'>
-      <h1 className='text-lg font-semibold text-accent md:text-xl lg:text-2xl'>
+      <h1 className='text-accent text-lg font-semibold md:text-xl lg:text-2xl'>
         {pet?.name}
       </h1>
       <div>
         <div className='mt-10 flex items-center gap-1'>
-          <CalendarBlank size={18} className='text-accent' weight='bold' />
-          <span className='ms-2 text-content-300'>
+          <CalendarBlankIcon size={18} className='text-accent' weight='bold' />
+          <span className='text-content-300 ms-2'>
             {birthDateToHumanReadableAge(pet.birthDate)}
           </span>
         </div>
         <div className='mt-5 flex items-center gap-1'>
-          <MapPin size={18} className='text-accent' weight='bold' />
-          <span className='ms-2 text-content-300'>{pet.location}</span>
+          <MapPinIcon size={18} className='text-accent' weight='bold' />
+          <span className='text-content-300 ms-2'>{pet.location}</span>
         </div>
         <div className='mt-5 flex items-center gap-1'>
           <Image
@@ -39,9 +39,9 @@ export default function PetDetailsCard({ pet }: PetDetailsCardProps) {
             alt='scale icon'
             className='text-accent'
           />
-          <span className='ms-2 text-content-300'>{pet.weight} kg</span>
+          <span className='text-content-300 ms-2'>{pet.weight} kg</span>
         </div>
-        <p className='mt-7 text-content-300'>{pet.description}</p>
+        <p className='text-content-300 mt-7'>{pet.description}</p>
         <div className='flex items-center justify-center'>
           <Link
             href={`/pet/adoption?id=${pet.id}&name=${pet.name}&gender=${pet.gender}`}
@@ -50,7 +50,7 @@ export default function PetDetailsCard({ pet }: PetDetailsCardProps) {
               aria-label='Solicitar adoção'
               label='Solicitar adoção'
               className='mt-7 w-72'
-              icon={<PawPrint size={24} />}
+              icon={<PawPrintIcon size={24} />}
             />
           </Link>
         </div>
